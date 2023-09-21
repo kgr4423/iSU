d3fd4374bf74a134f5746622d4eac0b5b686a066
kgr4423 <weak0496@gmail.com>
2023-09-21 18:18:15 +0900

    add antisitsystem

---
+++ antiSitSystem/antiSitSystem.ino
@@ -0,0 +1,52 @@
+#include <SDHCI.h>
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <cstring>
+#include <Camera.h>
+#include <RTC.h>
+
+#define TIME_HEADER 'T' // Header tag for serial time sync message
+#define BAUDRATE                (115200)
+#define TOTAL_PICTURE_COUNT     (5)
+
+SDClass  theSD;
+int sitCount = 0;
+char mode = "safe"
+
+void setup()
+{  
+  SerialPortSetup();
+  SdSetup();
+  SdUsbMscSetup();
+  RtcSetup();
+  CameraSetup();
+
+  writeLogFile("Setup done");
+}
+
+void loop()
+{
+  //時刻の同期
+  RtcSynchronize();
+
+  //静止画像を撮影するまでX秒待つ
+  sleep(60);  //60秒
+
+  //静止画像の撮影と保存、画像の保存先パスを返す
+  char* imagePath = takeAndSavePicture();
+
+  //画像中の人の有無識別
+  bool personDetected = detectPersonInImage(imagePath);
+
+  //カウンタの更新
+  sitCountDiff = sitCountUpdater(personDetected);
+  sitCount = sitCount + sitCountDiff;
+
+  //カウンタに応じたモード判定
+  mode = determineMode(sitCount);
+
+  //モードに応じた警告など
+  alert(mode);
+
+}
