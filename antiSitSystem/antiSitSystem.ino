006a168d7d563b3b1671c8734e69c59c0578e1e3
kgr4423 <weak0496@gmail.com>
2023-09-25 17:09:40 +0900

    add camera_interval

--- antiSitSystem/antiSitSystem.ino
+++ antiSitSystem/antiSitSystem.ino
@@ -8,9 +8,9 @@
 
 #define TIME_HEADER 'T' // Header tag for serial time sync message
 #define BAUDRATE                (115200)
-#define TOTAL_PICTURE_COUNT     (5)
 
 SDClass  theSD;
+int camera_interval = 60;
 int sitCount = 0;
 char mode = "safe"
 
@@ -28,7 +28,7 @@ void setup()
 void loop()
 {
   //静止画像を撮影するまでX秒待つ
-  sleep(60);
+  sleep(camera_interval);
 
   //静止画像の撮影と保存、画像の保存先パスを返す
   char* imagePath = takeAndSavePicture();
@@ -37,7 +37,8 @@ void loop()
   bool personDetected = detectPersonInImage(imagePath);
 
   //カウンタの更新
-  sitCountUpdater(&sitCount, personDetected);
+  int delta = sitCountUpdater(mode, personDetected);
+  sitCount = sitCount + delta;
 
   //カウンタに応じたモード判定
   mode = determineMode(sitCount);
