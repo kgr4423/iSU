ef2960bc77dbc8c2fb5db5a622a1c94223f60926
kgr4423 <weak0496@gmail.com>
2023-11-29 23:10:56 +0900

    コメント追加と不要部分削除

--- antiSitSystem/func_display.ino
+++ antiSitSystem/func_display.ino
@@ -51,13 +51,14 @@ void display_image(uint16_t* buf, bool result) {
     int n = 0; 
     for (int y = z*10; y < z*10 + 10; ++y) {
       for (int x = 160; x > 0; --x) {
-        // 撮影画像表示部分
+        // YUV422形式の画像データから輝度成分を抽出しRGB565へ変換
         uint16_t value = buf[y*160 + x];
         uint16_t y_h = (value & 0xf000) >> 8;
         uint16_t y_l = (value & 0x00f0) >> 4;
-        value = (y_h | y_l);       
+        value = (y_h | y_l);
         uint16_t value6 = (value >> 2);
         uint16_t value5 = (value >> 3);
+        // 撮影画像表示部分
         disp[n] = (value5 << 11) | (value6 << 5) | value5;
         disp[n+1] = (value5 << 11) | (value6 << 5) | value5;
         disp[320+n] = (value5 << 11) | (value6 << 5) | value5;
@@ -80,13 +81,14 @@ void display_image(uint16_t* buf, bool result) {
           disp[320+n] = ILI9341_RED;
           disp[320+n+1] = ILI9341_RED;
         }
+        // 画像データの読み出し番号更新
         n = n + 2;
         if (n % 320 == 0){
           n = n + 320;
         }
-        disp[n] = (value5 << 11) | (value6 << 5) | value5;
       }
     }
+    // 画像の表示
     tft.drawRGBBitmap(0, z*20, disp, 320, 20); 
   }
 }
