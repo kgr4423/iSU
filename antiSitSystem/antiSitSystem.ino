fdac7a9ac8aa9b1c4e41b89faa7364203d5c5a75
kgr4423 <weak0496@gmail.com>
2023-12-03 17:12:33 +0900

    width height

--- antiSitSystem/antiSitSystem.ino
+++ antiSitSystem/antiSitSystem.ino
@@ -1,4 +1,4 @@
-/* 推論処理関連*/
+// 推論処理関連
 #include "tensorflow/lite/micro/all_ops_resolver.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
@@ -17,17 +17,17 @@ int inference_count = 0;
 constexpr int kTensorArenaSize = 100000;
 uint8_t tensor_arena[kTensorArenaSize];
 
-/* カメラの撮影・撮影画像の表示関連 */
+// カメラの撮影・撮影画像の表示関連
 #include <Camera.h>
-const int offset_x = 25;
-const int offset_y = 5;
-const int width = 160;
-const int height = 120;
+const int offset_x = 16;
+const int offset_y = 16;
+const int width = 110;
+const int height = 110;
 const int target_w = 110;
 const int target_h = 110;
 const int pixfmt = CAM_IMAGE_PIX_FMT_YUV422;
 
-/* その他 */
+// その他
 const int beep_pin = 14; // 音声出力ピンの設定
 int sitCount = 0;
 int last_mode = 4;
@@ -38,35 +38,35 @@ int danger_width = 30;
 int person_score;
 int no_person_score;
 
-/* カメラストリームのコールバック関数 */
-/* カメラで画像がキャプチャされる度にこの関数が呼ばれる */
-/* 人認識の推論もこの関数内で行われる */
+// カメラストリームのコールバック関数
+// カメラで画像がキャプチャされる度にこの関数が呼ばれる
+// 人認識の推論もこの関数内で行われる
 void CamCB(CamImage img)
 {
     static uint32_t last_mills = 0;
 
-    /* キャプチャ画像データの取得 */
+    // キャプチャ画像データの取得
     uint16_t *buf = getImageData(img);
 
-    /* 人認識用に画像データを整形しTensorFlowの入力バッファにセット */
+    // 人認識用に画像データを整形しTensorFlowの入力バッファにセット
     setImageForPersonDetection(buf);
-    /* 人の有無判定 */
+    // 人の有無判定
     bool personDetected = detectPersonInImage();
 
-    /* カウンタの更新 */
+    // カウンタの更新
     int delta = sitcountUpdater(personDetected);
     sitCount = sitCount + delta;
-    /* モードの更新 */
+    // モードの更新
     determineMode(safe_width, attention_width, danger_width);
 
-    /* キャプチャ画像の表示 */
+    // キャプチャ画像の表示
     display_image(buf, personDetected);
-    /* 各種パラメータの表示 */
+    // 各種パラメータの表示
     displayText();
-    /* 警告処理 */
+    // 警告処理
     alert();
 
-    /* 処理時間の測定と表示 */
+    // 処理時間の測定と表示
     uint32_t current_mills = millis();
     uint32_t duration = current_mills - last_mills;
     Serial.println("duration = " + String(duration));
--- antiSitSystem/func_display.ino
+++ antiSitSystem/func_display.ino
@@ -45,15 +45,15 @@ void displayText()
 void display_image(uint16_t *buf, bool result)
 {
 
-    for (int z = 0; z < 12; ++z)
+    for (int z = 0; z < height/10; ++z)
     {
         int n = 0;
         for (int y = z * 10; y < z * 10 + 10; ++y)
         {
-            for (int x = 160; x > 0; --x)
+            for (int x = width; x > 0; --x)
             {
                 // YUV422形式の画像データから輝度成分を抽出しRGB565へ変換
-                uint16_t value = buf[y * 160 + x];
+                uint16_t value = buf[y * width + x];
                 uint16_t y_h = (value & 0xf000) >> 8;
                 uint16_t y_l = (value & 0x00f0) >> 4;
                 value = (y_h | y_l);
@@ -62,15 +62,15 @@ void display_image(uint16_t *buf, bool result)
                 // 撮影画像表示部分
                 disp[n] = (value5 << 11) | (value6 << 5) | value5;
                 disp[n + 1] = (value5 << 11) | (value6 << 5) | value5;
-                disp[320 + n] = (value5 << 11) | (value6 << 5) | value5;
-                disp[320 + n + 1] = (value5 << 11) | (value6 << 5) | value5;
+                disp[width*2 + n] = (value5 << 11) | (value6 << 5) | value5;
+                disp[width*2 + n + 1] = (value5 << 11) | (value6 << 5) | value5;
                 if (result && (10 <= y) && (y <= 20) && (110 <= x) && (x <= 130))
                 {
                     // 判定アイコン表示部分
                     disp[n] = ILI9341_RED;
                     disp[n + 1] = ILI9341_RED;
-                    disp[320 + n] = ILI9341_RED;
-                    disp[320 + n + 1] = ILI9341_RED;
+                    disp[width*2 + n] = ILI9341_RED;
+                    disp[width*2 + n + 1] = ILI9341_RED;
                 }
                 if (x == offset_x || x == width - offset_x || y == offset_y || y == height - offset_y)
                 {
@@ -82,18 +82,18 @@ void display_image(uint16_t *buf, bool result)
                     // 警告枠表示部分
                     disp[n] = ILI9341_RED;
                     disp[n + 1] = ILI9341_RED;
-                    disp[320 + n] = ILI9341_RED;
-                    disp[320 + n + 1] = ILI9341_RED;
+                    disp[width*2 + n] = ILI9341_RED;
+                    disp[width*2 + n + 1] = ILI9341_RED;
                 }
                 // 画像データの読み出し番号更新
                 n = n + 2;
-                if (n % 320 == 0)
+                if (n % (width*2) == 0)
                 {
-                    n = n + 320;
+                    n = n + width*2;
                 }
             }
         }
         // 画像の表示
-        tft.drawRGBBitmap(0, z * 20, disp, 320, 20);
+        tft.drawRGBBitmap(offset_x, z * 20 + offset_y, disp, width*2, 20);
     }
 }
