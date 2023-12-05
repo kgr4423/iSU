921aafd4449007bfb94d98b685536c6913f34cd3
kgr4423 <weak0496@gmail.com>
2023-12-05 22:58:38 +0900

    a

--- number_recognition/number_recognition.ino
+++ number_recognition/number_recognition.ino
@@ -31,12 +31,16 @@ SDClass theSD;
 #define TFT_DC 9
 #define TFT_CS 10
 
+// 推論画像サイズ
 #define DNN_IMG_W 28
 #define DNN_IMG_H 28
+// 撮影画像サイズ
 #define CAM_IMG_W 320
 #define CAM_IMG_H 240
+// 目標範囲の左上角座標
 #define CAM_CLIP_X 104
 #define CAM_CLIP_Y 0
+// 目標範囲の幅と高さ
 #define CAM_CLIP_W 112
 #define CAM_CLIP_H 224
 
@@ -50,19 +54,6 @@ DNNRT dnnrt;
 DNNVariable input(DNN_IMG_W *DNN_IMG_H);
 
 static uint8_t const label[11] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-void putStringOnLcd(String str, int color)
-{
-    int len = str.length();
-    tft.fillRect(0, 224, 320, 240, ILI9341_BLACK);
-    tft.setTextSize(2);
-    int sx = 160 - len / 2 * 12;
-    if (sx < 0)
-        sx = 0;
-    tft.setCursor(sx, 225);
-    tft.setTextColor(color);
-    tft.println(str);
-}
 
 void CamCB(CamImage img)
 {
@@ -120,7 +111,6 @@ void CamCB(CamImage img)
     img.convertPixFormat(CAM_IMAGE_PIX_FMT_RGB565);
     uint16_t *imgBuf = (uint16_t *)img.getImgBuff();
     tft.drawRGBBitmap(0, 0, (uint16_t *)img.getImgBuff(), 320, 224);
-    putStringOnLcd(gStrResult, ILI9341_YELLOW);
 }
 
 void setup()
@@ -132,14 +122,14 @@ void setup()
 
     while (!theSD.begin())
     {
-        putStringOnLcd("Insert SD card", ILI9341_RED);
+        Serial.println("insert sd card");
     }
 
     File nnbfile = theSD.open("model.nnb");
     int ret = dnnrt.begin(nnbfile);
     if (ret < 0)
     {
-        putStringOnLcd("dnnrt.begin failed" + String(ret), ILI9341_RED);
+        Serial.println("dnnrt.begin failed");
         return;
     }
 
